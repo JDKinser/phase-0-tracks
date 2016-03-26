a7d3b03cf7e1c03be92c0ad99e479f592a18ff02
Jesse Kinser <kinserjesse@gmail.com>
2016-03-26 16:44:45 -0700

    added questions and logic

---
+++ ruby/vampires.rb
@@ -0,0 +1,27 @@
+# Using time class so program doesnt need to be updated every year.
+currentYear = Time.new.year
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+dob = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+bread = gets.chomp.downcase
+
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+aflac = gets.chomp.downcase
+
+if age == (currentYear - dob) && (bread == "y" || aflec == "y")
+	puts "Probably not a vampire"
+elsif age != (currentYear - dob) && (bread == "n" || aflec == "n")
+	puts "Probably a vampire"
+elsif age != (currentYear - dob) && (bread == "n" && aflec == "n")
+	puts "Almost certainly a vampire"
+elsif name == "Drake Cula" || name == "Tu Fang"
+	puts "Definitely a vampire"
+else puts "Results inconclusive"
