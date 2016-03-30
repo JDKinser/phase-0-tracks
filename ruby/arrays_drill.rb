fc223e257539aa3626af93e723b9445a82253a6d
Jesse Kinser <kinserjesse@gmail.com>
2016-03-30 15:48:26 -0700

    added array

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -3,3 +3,12 @@ p movies
 
 movies = movies + ["Fight Club", "Life Aquatic", "Royal Tennenbaums", "Scarface", "TRON"]
 p movies
+
+movies.delete_at(2)
+p movies
+
+movies.insert(2, "Pulp Fiction")
+p movies
+
+movies.delete("Fight Club")
+p movies
