56cad45ec9d86ca14ac8ff9b469a77fdccd9ba40
Jesse Kinser <kinserjesse@gmail.com>
2016-04-01 15:04:35 -0700

    added bottom drawer and fixed errors

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,31 +1,42 @@
 dresser = {
-	top_drawer {
-		cotents: "socks",
-		sock_count {
+	top_drawer: {
+		cotents: 'socks',
+		sock_count: {
 			black: 8,
 			white: 10,
 			brown: 2
-			}
-		sock_type [
+			},
+		sock_type: [
 			"paired",
 			"single",
 			"holes"
 			]
-		}
-	middle_drawer {
-		cotents: "shirts",
-		shirt_count {
+		},
+	middle_drawer: {
+		cotents: 'shirts',
+		shirt_count: {
 			t_shirt: 5,
 			button_up: 3,
 			v_neck: 2
-			}
-		shirt_type [
+			},
+		shirt_type: [
 			"favorite",
 			"new",
 			"stained"
 			]
-		}
-	bottom_drawer {
-		
+		},
+	bottom_drawer: {
+		cotents: 'pants',
+		pants_count: {
+			jeans: 6,
+			slacks: 2,
+			sweat: 4
+			},
+		pants_type: [
+			"tight",
+			"loose",
+			"favorite",
+			"faded"
+			]
 		}
 	}
