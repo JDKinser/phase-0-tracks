41213b6c28574a977c6b935040e2dbd912b9076a
Jesse Kinser <kinserjesse@gmail.com>
2016-04-01 11:31:39 -0700

    Streamlined the input process

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,44 +1,30 @@
 #Interior Design Info Form
-def self.to_b(str)
-	case str
-	when "y" then true
-	when "n" then false
-	end
-end
 
 #Create an empty hash
 user_info = {}
 
 #Ask user for name.
-#Store input to name variable.
-#Add variable as a value to the "name" key.
+#Store input to key :name in hash.
 print "Please enter your full name: "
-name = gets.chomp
-user_info[:name] = name
+user_info[:name] = gets.chomp
 
 
 #Ask user for age.
-#Store input to age variable as an integer.
-#Add variable as a value to the "age" key.
+#Store input to key :age in hash.
 print "Please enter your age: "
-age = gets.chomp.to_i
-user_info[:age] = age
+user_info[:age] = gets.chomp.to_i
 
 
 #Ask user for number of children.
-#Store input to kids variable as an integer.
-#Add variable a value to the "kids" key.
+#Store input to key :kids in hash.
 print "How many children do you have? (You may include your husband): "
-kids = gets.chomp.to_i
-user_info[:kids] = kids
+user_info[:kids] = gets.chomp.to_i
 
 
 #Ask user what their dream decor theme.
-#Store input to dream_decor variable.
-#Add variable as a value to the "dream_decor" key.
+#Store input to key :dream_decor in hash.
 print "What is your dream decors theme?: "
-dream_decor = gets.chomp
-user_info[:dream_decor] = dream_decor
+user_info[:dream_decor] = gets.chomp
 
 
 #Ask user if they own their home.
