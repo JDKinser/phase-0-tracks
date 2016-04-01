17b111003e9836f9d49ac506e93ef7e79cef49a2
Jesse Kinser <kinserjesse@gmail.com>
2016-04-01 14:29:35 -0700

    added top drawer

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,21 @@
+dresser = {
+	top_drawer {
+		cotents: "socks",
+		sock_count {
+			black: 8,
+			white: 10,
+			brown: 2,
+			}
+		sock_type [
+			"paired",
+			"single"
+			"holes"
+			]
+		}
+	middle_drawer {
+		
+		}
+	bottom_drawer {
+		
+		}
+	}
