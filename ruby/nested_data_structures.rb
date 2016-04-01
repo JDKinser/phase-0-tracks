daaca5c7da8f7bbd6e014110dd18e10fe89ca496
Jesse Kinser <kinserjesse@gmail.com>
2016-04-01 14:32:21 -0700

    added middle drawer

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,16 +4,26 @@ dresser = {
 		sock_count {
 			black: 8,
 			white: 10,
-			brown: 2,
+			brown: 2
 			}
 		sock_type [
 			"paired",
-			"single"
+			"single",
 			"holes"
 			]
 		}
 	middle_drawer {
-		
+		cotents: "shirts",
+		shirt_count {
+			t_shirt: 5,
+			button_up: 3,
+			v_neck: 2
+			}
+		shirt_type [
+			"favorite",
+			"new",
+			"stained"
+			]
 		}
 	bottom_drawer {
 		
