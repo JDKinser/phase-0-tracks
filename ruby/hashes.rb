528a5cad6c1c48fe58b201993e37406ac79602ee
Jesse Kinser <kinserjesse@gmail.com>
2016-03-31 17:02:50 -0700

    added initial pseudocode

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,1 +1,7 @@
-#pseudocode
+#Interior Design Info Form
+#Create an empty hash
+#Ask user for name.
+#Ask user for age.
+#Ask user for number of children.
+#Ask user what their dream decor theme.
+#Ask user if they own their home.
