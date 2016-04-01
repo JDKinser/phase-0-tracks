a69346562eaadc194e2af4d93cc7b4087680f7bb
Jesse Kinser <kinserjesse@gmail.com>
2016-03-31 17:14:19 -0700

    added code to gather name and add to hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,6 +1,17 @@
 #Interior Design Info Form
 #Create an empty hash
+user_info = {}
+
 #Ask user for name.
+#Store input to name variable.
+#Add variable as value to the "name" key.
+print "Please enter your full name: "
+name = gets.chomp
+user_info[:name] = name
+
+
+
+
 #Ask user for age.
 #Ask user for number of children.
 #Ask user what their dream decor theme.
