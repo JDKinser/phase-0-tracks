31a5b8d3fe4e7a4bf81333ac170c07614a62ce2e
Jesse Kinser <kinserjesse@gmail.com>
2016-04-01 23:58:26 -0700

    Added information to program

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,8 +3,13 @@
 # Define a method to scramble given name.
 def scrambler (name)
 	swapped = name.split.rotate.join(" ") # Take name and flip it.
-	swapped.downcase!  # Make the both names lowercase.
+	swapped.downcase!  # Make string lowercase.
 	name1 = swapped.tr("aeiou", "eioua") # Shift all vowels by 1 place.
 	name2 = name1.tr("bcdfghjklmnpqrstvwxyz", "cdfghjklmnpqrstvwxyzb") # Shift all conconants by 1 place.
 	name3 = name2.split.map(&:capitalize).join(" ") # Make string into substrings capitalize and join back.
 end
+
+
+puts "Thank you for using Name Scrambler 2000." # Greet User.
+puts "After you have input your name type 'quit' to exit Name Scrambler 2000." # Explain exit strategy. 
+print "Please enter your First and Last name: " # Ask user for First and last name.
