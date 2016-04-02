00ea1e168e25478f0b134bb66ca50c53ddb2f1fe
Jesse Kinser <kinserjesse@gmail.com>
2016-04-01 23:53:40 -0700

    defined method to scramble name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,4 +1,10 @@
 # Name Scrambler 2000
 
 # Define a method to scramble given name.
-"jesse kinser".split.rotate.join(" ")
+def scrambler (name)
+	swapped = name.split.rotate.join(" ") # Take name and flip it.
+	swapped.downcase!  # Make the both names lowercase.
+	name1 = swapped.tr("aeiou", "eioua") # Shift all vowels by 1 place.
+	name2 = name1.tr("bcdfghjklmnpqrstvwxyz", "cdfghjklmnpqrstvwxyzb") # Shift all conconants by 1 place.
+	name3 = name2.split.map(&:capitalize).join(" ") # Make string into substrings capitalize and join back.
+end
