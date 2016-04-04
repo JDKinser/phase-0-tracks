1dae7de8d5885319e7cc793167431c74edee0468
Jesse Kinser <kinserjesse@gmail.com>
2016-04-03 22:42:49 -0700

    Added method

--- practice_assessment/calculator.rb
+++ practice_assessment/calculator.rb
@@ -1,1 +1,12 @@
 # Practice
+# write a method that takes three parameters:
+# an integer, an operator string (example: "+"), and another integer
+
+# I cant figure out how to add the operator string in the time alloted so I am going to go for a longer route.
+
+# define method that takes two integers and makes array.
+
+def calculate
+  [n1,n2].map{ |x| x.chomp.to_i }
+end
+
