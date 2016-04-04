148966136bce6328b943faa749b73c582ef9e632
Jesse Kinser <kinserjesse@gmail.com>
2016-04-03 22:58:08 -0700

    Added program exit

--- practice_assessment/calculator.rb
+++ practice_assessment/calculator.rb
@@ -6,7 +6,22 @@
 
 # define method that takes two integers and makes array.
 
-def calculate
-  [n1,n2].map{ |x| x.chomp.to_i }
-end
+#def calculate
+#  [n1,n2].map{ |x| x.chomp.to_i }
+#end
 
+print "Are you going to be 'adding', 'subtreacting', 'multiplying' or 'dividing': "
+answer = gets.chomp.downcase
+
+status = false
+
+until status == true
+puts "Thank you for using our calculator."
+puts "Type 'done' when you are through with the calculator."
+print "Are you going to be 'adding', 'subtreacting', 'multiplying' or 'dividing': "
+answer = gets.chomp.downcase
+	if answer == 'done'
+		puts "Thank you for using the calculator we hope to see you again soon."
+		status = true
+	end
+end
