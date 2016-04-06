24fed6d940eec550a224275c85b77ef223b23b96
Jesse Kinser <kinserjesse@gmail.com>
2016-04-05 22:21:33 -0700

    added santa class and started adding methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,1 +1,16 @@
 # Santa class
+
+# Make Santa class
+class Santa
+	def initialize
+		puts "Initializing Santa instance..."
+	end
+	
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+	
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+end
