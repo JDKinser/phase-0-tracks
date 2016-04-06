62204d6b6d08fc88ea46df1864151b8c7206e266
Jesse Kinser <kinserjesse@gmail.com>
2016-04-05 20:18:41 -0700

    filled out form

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin
+SPECIES ------------------------
+Puppious Verycuteus
+
+CHARACTERISTICS ----------------
+Whisker length: varies
+Tail lenght: varies
+Friendly: varies
+Bark volume: varies
+Name: varies
+Coat color: varies
+Paw count: 4
+Slober level: varies
+
+BEHAVIOR -----------------------
+Unconditional companion
+Chases tail
+Unending kisses
+Cuddle champion
+=end
