b5ccc28abec7091db2d2e44bbf67c2086c02cdba
Jesse Kinser <kinserjesse@gmail.com>
2016-04-07 13:55:15 -0700

    Added gender and ethnicity arrays

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -38,7 +38,47 @@ class Santa
 	
 end
 
+gender = ["agender", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicity = ["Black", "Latino", "White", "Asian", "Pacific Islander","Prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+
+
+
+
+=begin
 santa = Santa.new("male","white") # Store and initialize Santa class in variable "santa"
 santa.speak  #run speak method.
 santa.eat_milk_and_cookies("oreo")  #run EMC method.
 santa.about #driver call to check attributes
+=end
+
+
+
+#class Puppy
+#  def initialize(name)
+#  	@name = name
+#  end
+	
+#  def bark
+#  	puts "#{@name} says: Woof!"
+#  end
+#end
+
+# we make an array of names to build puppies with
+#names = ["Fido", "Spot", "Duchess", "Ginger"]
+
+# we make an empty container for our puppy collection
+#puppies = []
+
+#puts "Iterating through names list to create puppies ..."
+#names.each do |name|
+#  puts "Creating a puppy named #{name} ..."
+#  puppies << Puppy.new(name)
+#  puts "There are now #{puppies.length} Puppy instances in the array"
+#  puts "----"
+#end
+
+#puts "Testing each Puppy instance in the array to make sure it can bark ..."
+#puppies.each do |puppy|
+#  puppy.bark
+#end
