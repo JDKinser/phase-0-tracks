73992061ca3b50e1931f403cbb880079097288b2
Jesse Kinser <kinserjesse@gmail.com>
2016-04-08 15:50:10 -0700

    Added to pseudo-code

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,6 +1,11 @@
 #Class Solo Challenge
 
 # Make Airplane class
+# add attr_reader and attr_accessor
 # add initialization
 # add 3 attributes to initialization 1 with argument
 # build three methods
+
+# ask user how many planes they would like to rent
+# add each instance to an array
+# loop through the array and output info
