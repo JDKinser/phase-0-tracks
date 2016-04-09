3f2592613002dee9d83493ecf7abe8b899c004c3
Jesse Kinser <kinserjesse@gmail.com>
2016-04-08 21:19:10 -0700

    Started user interface

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -43,11 +43,51 @@ class Airplane
 	
 end
 
+
+planes = []
+
+puts "Welcome to your very own airport."
+puts "Please follow the instructions so we can gets these planes in the air."
+puts "When you are done sending your planes all over the world type 'done'"
+
+counter = 0
+
+until counter == 1
+	print "What is the name of your airplane? "
+	name = gets.chomp
+
+	if name.downcase == "done"
+		puts "Here is a list of your airplanes and thier info!"
+		planes.each do |plane|
+			p planes[plane]	
+		end
+		counter += 1
+	else
+		print "Where will we be taking off from today? "
+		location = gets.chomp
+		
+		print "Where we be heading today? "
+		destination = gets.chomp
+		
+		airplane = Airplane.new(name, destination)
+		airplane.current_location(location)
+		puts "The planes name is: #{airplane.name}"
+		puts "Its destination is: #{airplane.destination}"
+		puts "there are #{airplane.guests} guests on the plane."
+		airplane.turbulence("sunny")
+	end
+end
+
+
+
+
+
+
 # Driver Code
 
-airplane = Airplane.new("flock", "Florda")
-airplane.current_location("arizona")
-puts "The planes name is: #{airplane.name}"
-puts "Its destination is: #{airplane.destination}"
-puts "there are #{airplane.guests} guests on the plane."
-airplane.turbulence("sunny")
+#airplane = Airplane.new("flock", "Florda")
+#airplane.current_location("arizona")
+#puts "The planes name is: #{airplane.name}"
+#puts "Its destination is: #{airplane.destination}"
+#puts "there are #{airplane.guests} guests on the plane."
+#airplane.turbulence("sunny")
