46cbe3d2e92359cf20859c92231d889cdd789a50
Jesse Kinser <kinserjesse@gmail.com>
2016-04-08 23:44:56 -0700

    added new shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,24 @@
+#module Shout
+#	
+#	def self.yell_angrily(words)
+#		words + "!!!" + " :("
+#	end
+#	
+#	def self.yell_happily(words)
+#		words + "!!!" + " :)" # took the easy way out... SHHH
+#	end
+#	
+#end
+#
+#p Shout.yell_happily("I love it")
+#
+#p Shout.yell_angrily("I Lied")
+
+
 module Shout
 	
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
-	
-	def self.yell_happily(words)
-		words + "!!!" + " :)" # took the easy way out... SHHH
+	def yell(words)
+		words.upcase + "!!!"
 	end
 	
 end
-
-p Shout.yell_happily("I love it")
-
-p Shout.yell_angrily("I Lied")
