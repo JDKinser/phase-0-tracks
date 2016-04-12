e59438a20195ba45bcfc48e8c3e01fe5e2764054
Jesse Kinser <kinserjesse@gmail.com>
2016-04-11 19:22:47 -0700

    Pseudo-code for release 0

---
+++ js/algos.js
@@ -0,0 +1,9 @@
+/*
+Release 0
+Create a function that will input an array as an argument.
+input array
+split array
+loop through new arrays 
+call .lenght on array
+Return the longest string within the array.
+*/
