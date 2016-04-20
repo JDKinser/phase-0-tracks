21617cd282dac9be9ed1515140ad729d97e02422
Jesse Kinser <kinserjesse@gmail.com>
2016-04-19 18:22:12 -0700

    Added methods to add, delete and update DB

--- databases/car_sales_app/car_sales.rb
+++ databases/car_sales_app/car_sales.rb
@@ -29,3 +29,55 @@ SQL
 # create a main_list table (if it's not there already)
 db.execute(create_cars_table)
 db.execute(create_makes_table)
+
+# *****************************************************************************
+
+# method to add a listing
+def add_listing(db, seller, model, year, price)
+  db.execute("INSERT INTO cars (seller, model, year, price) VALUES (?, ?, ?, ?)", [seller, model, year, price])
+end
+
+# method to add a make
+def add_make(db, make)
+  db.execute("INSERT INTO makes (make) VALUES (?)", [make])
+end
+
+# method to delete a listing
+def delete_listing(db, id)
+	db.execute("DELETE FROM cars WHERE id=?", [id])
+end
+
+# method to delete a make
+def delete_make(db, id)
+  db.execute("DELETE FROM makes WHERE id=?", [id])
+end
+
+# method to update cars seller
+def update_seller(db, id, seller)
+  db.execute("UPDATE cars SET seller=? WHERE id=?", [seller, id]) 
+end
+
+# method to update cars model
+def update_model(db, id, model)
+  db.execute("UPDATE cars SET model=? WHERE id=?", [model, id]) 
+end
+
+# method to update cars year
+def update_year(db, id, year)
+  db.execute("UPDATE cars SET year=? WHERE id=?", [year, id]) 
+end
+
+# method to update cars price
+def update_price(db, id, price)
+  db.execute("UPDATE cars SET price=? WHERE id=?", [price, id])
+end
+
+# method to update cars make
+def update_make (db, id, make)
+  db.execute("UPDATE makes SET make=? WHERE id=?", [make, id])
+end
+
+
+
+
+
