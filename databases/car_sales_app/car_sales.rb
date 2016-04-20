99d415e70c33ded075db8a70c74519b6ca72aaac
Jesse Kinser <kinserjesse@gmail.com>
2016-04-19 17:49:11 -0700

    Added sqlite3 gem set variable db with new database created command to build both tables

--- databases/car_sales_app/car_sales.rb
+++ databases/car_sales_app/car_sales.rb
@@ -1,1 +1,31 @@
 # Car Selling App
+
+# require gem
+require 'sqlite3'
+
+# create SQLite3 database and set it equal to the variable db
+db = SQLite3::Database.new("carsales.db")
+db.results_as_hash = true
+
+create_cars_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS cars(
+    id INTEGER PRIMARY KEY,
+    seller VARCHAR(255),
+    make_id INT,
+    model VARCHAR(255),
+    year INT,
+    price INT,
+    FOREIGN KEY (make_id) REFERENCES makes(id)
+  )
+SQL
+
+create_makes_table = <<-SQL
+    CREATE TABLE IF NOT EXISTS makes(
+    id INTEGER PRIMARY KEY,
+    make VARCHAR(255)
+  )
+SQL
+
+# create a main_list table (if it's not there already)
+db.execute(create_cars_table)
+db.execute(create_makes_table)
