61f08f43c2a7c4f0d312707839aae0699f33c290
Jesse Kinser <kinserjesse@gmail.com>
2016-04-30 16:01:20 -0700

    Cleaned up code

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -45,13 +45,21 @@ get '/students/:id' do
   student.to_s
 end
 
-# *** 9.4 work *** 
+
+####################
+# *** 9.4 work *** # 
+####################
+
+# make a contact route that displays an address.
 
 get '/contact' do
   "<br>Address:<br>
   1234 N Kitten Way<br>
   Catsville Meow 23412<br>"  
 end
+
+
+#make a great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
 
 get '/great_job' do
   name = params[:name]
@@ -62,12 +70,18 @@ get '/great_job' do
   end
 end
 
+
+# maek a route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
+
 get '/:num_1/plus/:num_2' do 
   x = params[:num_1].to_i
   y = params[:num_2].to_i
   sum = x + y
   "#{x} + #{y} = #{sum}"
 end
+
+
+# Optional bonus: Make a route that allows the user to search the database in some way -- maybe for students who have a certain first name, or some other attribute. If you like, you can simply modify the home page to take a query parameter, and filter the students displayed if a query parameter is present.
 
 get '/students/age/:age' do 
   students = db.execute("SELECT * FROM students WHERE age=?", [params[:age]])
@@ -80,19 +94,3 @@ get '/students/age/:age' do
   end
   response
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
